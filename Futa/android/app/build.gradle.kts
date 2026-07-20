f052dedb44aea896384b1da322b4a002dd18b592
jayzziebone <junior.lutula@gmail.com>
2026-07-20 16:18:45 -0400

    Update Android and iOS bundle identifier to com.futa.app

--- Futa/android/app/build.gradle.kts
+++ Futa/android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.futa.frontend"
+    namespace = "com.futa.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.futa.frontend"
+        applicationId = "com.futa.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
--- Futa/android/app/src/main/kotlin/com/futa/app/MainActivity.kt
+++ Futa/android/app/src/main/kotlin/com/futa/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.futa.frontend
+package com.futa.app
 
 import io.flutter.embedding.android.FlutterActivity
 
